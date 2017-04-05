31ebdf0534373b07e7b831dd14e793c81a9d9717
Artur Moczulski <artur.moczulski@gmail.com>
2017-04-05 16:34:30 +0000

    send more requests to Rollbar api

--- src/SampleRollbarPHP/App.php
+++ src/SampleRollbarPHP/App.php
@@ -4,6 +4,7 @@ namespace SampleRollbarPHP;
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\RollbarHandler;
 
 class App {
 
@@ -16,8 +17,6 @@ class App {
 		$this->log->pushHandler(new StreamHandler('php://stdout', Logger::INFO));
 
 		// Set up Rollbar
-		// require_once 'rollbar.php';
-
 		$config = array(
 		    // required
 		    'access_token' => 'eb2561a52efb4d4bba5a1d4b68be13e9',
@@ -27,6 +26,8 @@ class App {
 		    'root' => '/home/vagrant/Code/sample-rollbar-php'
 		);
 		\Rollbar::init($config);
+
+		// Set up Rollbar with Monolog
 	}
 
 	public function out(string $message)
@@ -37,8 +38,44 @@ class App {
 	public function run()
 	{
 
-		\Rollbar::report_message('testing 123', 'info');
-		$this->out("Sent message to rollbar");
+		$this->sendMessage("Test message sent to Rollbar with level 'info'.", "info");
+		$this->out("Sent message to Rollbar.");
 
+		$this->monolog("Test error reported through Monolog.");
+		$this->out("Logged and error with Monolog.");
+
+		$this->sendException("This is an exception explicitly sent to Rollbar.");
+		$this->out("Sent an exception to Rollbar.");
+
+		$this->out("Triggering an uncaught exception.");
+		$this->uncaughtException("This is an uncaught exception that should be automatically sent to Rollbar.");
+
+	}
+
+	public function sendMessage(string $msg, string $level = 'info')
+	{
+		\Rollbar::report_message($msg, $level);
+	}
+
+	public function sendException(string $msg)
+	{
+		try {
+			throw new \Exception($msg);
+		} catch(\Exception $e) {
+			\Rollbar::report_exception($e);
+		}
+	}
+
+	public function uncaughtException(string $msg)
+	{
+		throw new \Exception($msg);
+	}
+
+	public function monolog(string $msg)
+	{
+		$log = new Logger('SampleRollbarPHP\Monolog');
+		$log->pushHandler(new RollbarHandler(\Rollbar::$instance));
+
+		$log->error($msg);
 	}
 }
